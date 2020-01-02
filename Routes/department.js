43cd48e4fc8fa90db7770b537b2d42773ad8d65e
Anjalipatil18 <anjalip18@navgurukul.org>
2020-01-02 15:26:28 +0530

    GetDataById in departement table create Route

--- Routes/department.js
+++ Routes/department.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const department = express.Router()
 department.use(express.json())
-const departmentInsert = require('../Model/knex.js')
+const getDepartment = require('../Model/knex.js')
 
 department.get('/get',(req,res)=>{
-    let response = departmentInsert.selectData()
+    let response = getDepartment.selectData()
+    response.then((result)=>{
+        res.json(result)
+    }).catch((err)=>{
+        res.send(err)
+    });
+})
+
+department.get('/getDataById/:id',(req,res)=>{
+    var id=req.params.id
+    let response = getDepartment.selectDataById(id)
     response.then((result)=>{
         res.json(result)
     }).catch((err)=>{
