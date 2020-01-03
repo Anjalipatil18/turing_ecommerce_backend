aad54b6d976a55c4cd71de5ab7c3ffe5375caefe
Anjalipatil18 <anjalip18@navgurukul.org>
2020-01-03 12:46:27 +0530

    create new file get Attribute Table made route

---
+++ Routes/attribute.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const attribute = express.Router()
+attribute.use(express.json())
+const getcategories = require("../Model/attribute")
+
+attribute.get('/attributes',(req,res)=>{
+    let response = getcategories.selectData()
+    response.then((result)=>{
+        res.json(result)
+    }).catch((err)=>{
+        res.send(err)
+    });
+})
+
+attribute.get('/attributes/:attribute_id',(req,res)=>{
+    var attribute_id=req.params.attribute_id
+    let response = getcategories.selectDataById(attribute_id)
+    response.then((result)=>{
+        res.json(result)
+    }).catch((err)=>{
+        res.send(err)
+    });
+})
+
+attribute.get("/inProduct/:product_id",(req,res)=>{
+    let product_id=req.params.product_id
+    let response=getcategories.innerJoinData(product_id)
+    .then((data)=>{
+        res.send(data)
+    }).catch((err)=>{
+        res.send(err)
+    })  
+});
+
+// categories.get("/inDepartment/:department_id",(req,res)=>{
+//     let department_id=req.params.department_id
+//     let response=getcategories.joinTable(department_id)
+//     .then((data)=>{
+//         res.send(data)
+//     }).catch((err)=>{
+//         res.send(err)
+//     })  
+// });
+
+module.exports=attribute
--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 const knex = require("./Model/knex.js")
-// const category = require("./Model/categories")
+const category = require("./Model/categories")
+const attribute = require("./Model/attribute")
 
 const department = require('./Routes/department')
 app.use("/departments",department);
 
 const categories = require('./Routes/categories ')
 app.use("/categories",categories)
+
+const attributes = require('./Routes/attribute')
+app.use("/attributes",attributes)
 
 app.listen(2000,()=>{
     console.log("server is listening 2000.........")
