af400cdf21280612e22299f38aee5611a5145cee
Anjalipatil18 <anjalip18@navgurukul.org>
2020-01-07 23:39:35 +0530

    get data in shipping_regions and shipping table add

--- Model/shipping.js
+++ Model/shipping.js
@@ -15,8 +15,7 @@ let selectData = ()=>{
 }
 
 let selectDataById=(shipping_region_id)=>{
-    return knex.select("*").from("shipping_region").where("shipping_region_id",shipping_region_id)
+    return knex.select("shipping.shipping_id","shipping.shipping_type","shipping.shipping_cost","shipping_region.shipping_region_id").from("shipping_region").innerJoin("shipping").where("shipping_region.shipping_region_id",shipping_region_id)
 }
-
 
 module.exports={selectData,selectDataById}
